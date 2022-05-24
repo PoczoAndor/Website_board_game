d6dccd6d98072f5325f5454e0eded2b28d9371ca
PoczoAndor <poczogaming@gmail.com>
2022-05-24 18:04:30 +0300

    starting login system

--- Index.php
+++ Index.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Test Board Game</title>
-    <link rel="stylesheet" href="/css/style.css">
-  </head>
-  <body>
-<nav>
-  <div class="wrapper">
-    <ul>
-      <li><a href="">Home</a></li>
-      <li><a href="">About</a></li>
-      <li><a href="">Sign up</a></li>
-      <li><a href="">Log In</a></li>
-    </ul>
-  </div>
-</nav>
+<?php
+include_once'header.php';
+ ?>
   </body>
 </html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test Board Game</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <nav>
+      <div class="wrapper">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="login.php">Sign up</a></li>
+          <li><a href="signup.php">Log In</a></li>
+        </ul>
+      </div>
+    </nav>
