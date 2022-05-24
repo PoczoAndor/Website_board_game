0691a0e4557b13f5202f059db4380beb12200536
PoczoAndor <poczogaming@gmail.com>
2022-05-24 17:47:58 +0300

    Starting the website

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test Board Game</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+<nav>
+  <div class="wrapper">
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">Sign up</a></li>
+      <li><a href="">Log In</a></li>
+    </ul>
+  </div>
+</nav>
+  </body>
+</html>
