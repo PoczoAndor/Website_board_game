db9c90c19a54e93351b0c14c42c2a3a7c8b1b109
PoczoAndor <poczogaming@gmail.com>
2022-05-26 09:30:48 +0300

    sign up system error messages

--- signup.php
+++ signup.php
@@ -11,7 +11,40 @@ include_once'header.php';
      <input type="password" name="pwdrepeat" placeholder="Repeat Password...">
      <button type="submit" name="submit">Sign Upp</button>
    </form>
+   <?php
+   if (isset($_GET["error"]))
+   {
+     if ($_GET["error"]=="emptyinput") {
+       echo "<p>fill in all fields</p>";
+     }
+     else if($_GET["error"]=="invalidUid")
+     {
+       echo "<p>choose a proper username</p>";
+     }
+     else if($_GET["error"]=="invalidEmail")
+     {
+       echo "<p>choose a proper Email</p>";
+     }
+     else if($_GET["error"]=="passworddontmatch")
+     {
+       echo "<p>passwords dont match</p>";
+     }
+     else if($_GET["error"]=="statementfailed")
+     {
+       echo "<p>something went wrong</p>";
+     }
+     else if($_GET["error"]=="usernametaken")
+     {
+       echo "<p>username has been taken</p>";
+     }
+     else if($_GET["error"]=="none")
+     {
+       echo "<p>Sign upp succsessful</p>";
+     }
+   }
+    ?>
  </section>
+
 <?php
 include_once'footer.php';
  ?>
