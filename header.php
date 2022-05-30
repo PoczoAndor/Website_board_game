c8a64af897a5d8c119a8de0b97ea738cf89a0e4b
PoczoAndor <poczogaming@gmail.com>
2022-05-30 09:09:32 +0300

    Styling the navigation menu

--- header.php
+++ header.php
@@ -6,22 +6,22 @@ session_start();
   <head>
     <meta charset="utf-8">
     <title>Test Board Game</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
     <nav>
-      <div class="wrapper">
-        <ul>
-          <li><a href="Index.php">Home</a></li>
-          <li><a href="">About</a></li>
+      <div class="menu_wrapper">
+        <ul class="menu_ul">
+          <li class="menu_li"><a href="Index.php">Home</a></li>
+          <li class="menu_li"><a href="">About</a></li>
           <?php
            if (isset($_SESSION["useruid"])) {
              echo "<li><a href='profile.php'>Profile Page</a></li>";
              echo "<li><a href='includes/logout.inc.php'>Log Out</a></li>";
            }
            else {
-             echo "<li><a href='signup.php'>Home</a></li>";
-             echo "<li><a href='login.php'>Log In</a></li>";
+             echo "<li class='menu_li'><a href='signup.php'>Sign Up</a></li>";
+             echo "<li class='menu_li'><a href='login.php'>Log In</a></li>";
            }
            ?>
         </ul>
