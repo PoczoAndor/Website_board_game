b52fca50ad232e7632889ed64f9fb58de824a1c1
Andor <poczogaming@gmail.com>
2022-07-03 12:05:59 +0300

    test

--- Views/Index.php
+++ Views/Index.php
@@ -3,7 +3,7 @@ include_once'header.php';
 
  ?>
  <h1>Welcome friend to 'title of game'</h1>
- <p>You can choose to create and account and play the game to save your progres, or first test the game as a guest.</p><br>
+ <p>You can choose to create and account and play the game to save your progres, or first test the game as a guestt.</p><br>
 
 <?php
 include_once'footer.php';
